3c0422e7495f8a0a03b32fe5187e7abba3864b02
Neti <vneti77@gmail.com>
2022-03-04 12:02:13 +0200

    Adding fixes for the broken example of assignment

--- src/main/java/com/springframework/sfgdi/services/CatPetService.java
+++ src/main/java/com/springframework/sfgdi/services/CatPetService.java
@@ -1,8 +1,10 @@
 package com.springframework.sfgdi.services;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service("cat")
+@Profile("cat")
 public class CatPetService implements PetService {
     @Override
     public String getPetType() {
--- src/main/java/com/springframework/sfgdi/services/PetService.java
+++ src/main/java/com/springframework/sfgdi/services/PetService.java
@@ -1,5 +1,5 @@
 package com.springframework.sfgdi.services;
 
-public class PetService {
+public interface PetService {
     String getPetType();
 }
